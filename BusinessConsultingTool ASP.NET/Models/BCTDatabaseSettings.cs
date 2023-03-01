22fef47abcce8db7550237842bcaf95695b2f0de
Habeebullah Lawal <81315552+damnations@users.noreply.github.com>
2023-03-01 19:54:35 +0200

    create settings class to get db info

---
+++ BusinessConsultingTool ASP.NET/Models/BCTDatabaseSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace BusinessConsultingTool_ASP.NET.Models;
+
+public class BCTDatabaseSettings
+{
+    public string ConnectionString { get; set; } = null!;
+    public string DatabaseName { get; set; } = null!;
+    public string CollectionName { get; set; } = null!;
+}
