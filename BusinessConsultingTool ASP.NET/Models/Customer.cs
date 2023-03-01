4cd8d830008cdae62ccace44327f3598a34740df
Habeebullah Lawal <81315552+damnations@users.noreply.github.com>
2023-03-01 19:52:07 +0200

    create entity data model

---
+++ BusinessConsultingTool ASP.NET/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+
+namespace BusinessConsultingTool_ASP.NET.Models
+{
+    public class Customer
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+    }
+}
