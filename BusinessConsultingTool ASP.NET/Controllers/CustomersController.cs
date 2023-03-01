26eb9e36994955640351acf7512dcb8b1deba4f4
Habeebullah Lawal <81315552+damnations@users.noreply.github.com>
2023-03-01 19:55:35 +0200

    create service and controller for endpoints

---
+++ BusinessConsultingTool ASP.NET/Controllers/CustomersController.cs
@@ -0,0 +1,27 @@
+﻿using BusinessConsultingTool_ASP.NET.Models;
+using BusinessConsultingTool_ASP.NET.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BusinessConsultingTool_ASP.NET.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomersController: ControllerBase
+    {
+        private readonly CustomersService _customersService;
+
+        public CustomersController(CustomersService customersService) => _customersService = customersService;
+
+        [HttpGet]
+        public async Task<List<Customer>> Get() { }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Customer newCustomer) { }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> AddToPlaylist(string id, string movieId) { }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id) { }
+    }
+}
---
+++ BusinessConsultingTool ASP.NET/Services/CustomersService.cs
@@ -0,0 +1,23 @@
+﻿using BusinessConsultingTool_ASP.NET.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace BusinessConsultingTool_ASP.NET.Services
+{
+    public class CustomersService
+    {
+        private readonly IMongoCollection<Customer> _customerCollection;
+
+        public CustomersService(IOptions<BCTDatabaseSettings> bctDatabaseSettings)
+        {
+            MongoClient client = new MongoClient(bctDatabaseSettings.Value.ConnectionString);
+            IMongoDatabase database = client.GetDatabase(bctDatabaseSettings.Value.DatabaseName);
+            _customerCollection = database.GetCollection<Customer>(bctDatabaseSettings.Value.CollectionName);
+        }
+
+        public async Task<List<Customer>> GetAsync() { }
+        public async Task CreateAsync(Customer newCustomer) { }
+        public async Task AddToPlaylistAsync(string id, string movieId) { }
+        public async Task DeleteAsync(string id) { }
+    }
+}
