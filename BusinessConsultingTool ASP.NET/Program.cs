4a436ebff34c24229138846ff2302948603c4a37
Habeebullah Lawal <81315552+damnations@users.noreply.github.com>
2023-03-01 19:57:35 +0200

    get db settings and initialize singleton service

--- BusinessConsultingTool ASP.NET/Program.cs
+++ BusinessConsultingTool ASP.NET/Program.cs
@@ -1,6 +1,12 @@
+using BusinessConsultingTool_ASP.NET.Models;
+using BusinessConsultingTool_ASP.NET.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<BCTDatabaseSettings>(builder.Configuration.GetSection("BCTDatabase"));
+
+builder.Services.AddSingleton<CustomersService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
