40f4b22f84788108e617cccb493ef809c221a0b3
Habeebullah Lawal <81315552+damnations@users.noreply.github.com>
2023-06-15 02:41:50 +0300

    added xunit test

---
+++ tests/BusinessConsultingTool.DataAPI.UnitTests/Controllers/CustomersControllerTests.cs
@@ -0,0 +1,11 @@
+namespace BusinessConsultingTool.DataAPI.UnitTests.Controllers
+{
+    public class CustomersControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
